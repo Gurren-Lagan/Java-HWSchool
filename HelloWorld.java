59a27e9992cc4af77863b7a6ff226da2eacff7bb
Yair_GL <saint_seiya_1844@hotmail.com>
2017-10-22 17:16:00 -0500

    Add files via upload
    hw

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+//By Yair :v
+public class HelloWorld{
+    public String greeting;
+    public String name;
+
+public String salute(){
+      return greeting + ", " + name;
+    }
+
+public HelloWorld(String myGreeting, String myName){
+    greeting = myGreeting;
+    name = myName;
+}
+
+/*public HelloWorld(){
+    greeting = "Hello";
+    name = "uknown";
+}*/
+}
