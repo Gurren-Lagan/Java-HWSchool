c8a69e7a23ceec2f6b88a26df61db77412340058
Yair_GL <saint_seiya_1844@hotmail.com>
2017-10-23 02:56:58 -0500

    Update testSum.java

--- testSum.java
+++ testSum.java
@@ -1,11 +1,11 @@
-//clase de prueba para SumaMulti
+//clase de prueba para SumaMulti.java
 public class testSum
 {
 public static void main(String[] args) {
 
     SumaMulti suma = new SumaMulti();
 
-    suma.setNumero1(1);
+    suma.setNumero1(1); // pasas como parametros los numeros que deseas sumar es solo para probar :V
     suma.setNumero2(2);
     System.out.println("El resultado de la suma es: " + suma.Suma());
     System.out.println("El resultado de la multiplicacion es: " + suma.multiplicar());
