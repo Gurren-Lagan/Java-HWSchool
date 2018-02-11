1ddb275bce19f914064cdf0a22707c7cbffb51b1
Yair_GL <saint_seiya_1844@hotmail.com>
2018-02-11 10:37:21 -0600

    fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,16 @@ public class HelloWorld{
     public String greeting;
     public String name;
 
-public String salute(){
-      return greeting + ", " + name;
-    }
-
 public HelloWorld(String myGreeting, String myName){
     greeting = myGreeting;
     name = myName;
 }
+
+
+public String salute(){
+      return greeting + ", " + name;
+    }
+
 
 /*public HelloWorld(){
     greeting = "Hello";
