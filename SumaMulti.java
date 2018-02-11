8e3563f449d2b555ad02f09e6f9715479b12d028
Yair_GL <saint_seiya_1844@hotmail.com>
2018-02-11 10:40:16 -0600

    Fix

--- SumaMulti.java
+++ SumaMulti.java
@@ -19,23 +19,23 @@ public SumaMulti(int numero1, int numero2){
 	this.numero2=numero2;
 }
 
-
-/*Metodo suma() sin parametros
-Calcula la suma de 2 numeros enteros
-@return[entero]
-*/
-
-public int Suma(){
-	return numero1 + numero2;
-}
-
 public void setNumero1(int numero1){
 	this.numero1 = numero1;
 }
 
 public void setNumero2(int numero2){
 	this.numero2 = numero2;
-}
+}
+
+/*
+Metodo Suma() sin parametros
+Calcula la suma de 2 numeros enteros
+@return[entero]
+*/
+
+public int Suma(){
+	return numero1 + numero2;
+}
 
 public int multiplicar(){
 	return numero1 * numero2;
